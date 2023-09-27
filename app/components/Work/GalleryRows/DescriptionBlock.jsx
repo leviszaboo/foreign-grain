45351010bcacad782d983fe6476500941597e423
Levi <leviszaboo@icloud.com>
2023-09-27 12:43:47 +0200

    conditional rendering of description

--- app/components/Work/GalleryRows/DescriptionBlock.jsx
+++ app/components/Work/GalleryRows/DescriptionBlock.jsx
@@ -5,15 +5,21 @@ import { descriptionAnimationProps } from "./animation"
 export default function DescriptionBlock({title, subtitle, description}) {
   return (
     <motion.div className="block description-block" {...descriptionAnimationProps}>
-      <div className="image-title">
-        <h3>{title}</h3>
-      </div>
-      <div className="image-subtitle">
-        <h3>{subtitle}</h3>
-      </div>
-      <div className="image-description">
-        <h4>{description}</h4>
-      </div>
+      {title !== "" && (
+        <div className="image-title">
+          <h3>{title}</h3>
+        </div>
+      )}
+      {subtitle !== "" && (
+        <div className="image-subtitle">
+          <h3>{subtitle}</h3>
+        </div>
+      )}
+      {description !== "" && (
+        <div className="image-description">
+          <h4>{description}</h4>
+        </div>
+      )}
     </motion.div>
   )
 }
