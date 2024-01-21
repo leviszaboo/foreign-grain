7fadf22de4387f5511174d1a0a96ea21798c585b
Levi <leviszaboo@icloud.com>
2024-01-21 20:41:08 +0100

    open new tab when redirecting to socials

--- app/components/Contact/InfoBox.jsx
+++ app/components/Contact/InfoBox.jsx
@@ -13,12 +13,11 @@ export default function InfoBox({ contactInfo }) {
         <h2>{contactInfo.email}</h2>
         <h2>{contactInfo.phoneNumber}</h2>
         <h2>{contactInfo.address}</h2>
-        {/* <h2>Amsterdam, <span className="new-line">The Netherlands</span></h2> */}
         <div className="socials">
-          <Link href={"https://www.instagram.com/greenmario.film/"}>
+          <Link href={"https://www.instagram.com/greenmario.film/"} target="_blank">
             <Instagram color="white" size={32}/>
           </Link>
-          <Link href={"https://www.linkedin.com/in/luigi-simiani-65643b228/"}>
+          <Link href={"https://www.linkedin.com/in/luigi-simiani-65643b228/"} target="_blank">
             <Linkedin color="white" size={32}/>
           </Link>
         </div>
