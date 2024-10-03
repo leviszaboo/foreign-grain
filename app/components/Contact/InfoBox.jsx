6f3b8d32614f09bd1f4674e66c280d649bfc4ebc
Levi <leviszaboo@icloud.com>
2024-10-03 20:24:21 +0200

    feat: add customizable social media feature and dynamic metadata

--- app/components/Contact/InfoBox.jsx
+++ app/components/Contact/InfoBox.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -6,21 +6,20 @@ import { Instagram, Linkedin } from "lucide-react";
 import { infoBoxAnimationProps } from "./animation";
 
 export default function InfoBox({ contactInfo }) {
-
   return (
-      <motion.div className="info-box" {...infoBoxAnimationProps}>
-        <h2>{contactInfo.name}</h2>
-        <h2>{contactInfo.email}</h2>
-        <h2>{contactInfo.phoneNumber}</h2>
-        <h2>{contactInfo.address}</h2>
-        <div className="socials">
-          <Link href={"https://www.instagram.com/greenmario.film/"} target="_blank">
-            <Instagram color="white" size={32}/>
-          </Link>
-          <Link href={"https://www.linkedin.com/in/luigi-simiani-65643b228/"} target="_blank">
-            <Linkedin color="white" size={32}/>
-          </Link>
-        </div>
-      </motion.div>
-  )
+    <motion.div className="info-box" {...infoBoxAnimationProps}>
+      <h2>{contactInfo.name}</h2>
+      <h2>{contactInfo.email}</h2>
+      <h2>{contactInfo.phoneNumber}</h2>
+      <h2>{contactInfo.address}</h2>
+      <div className="socials">
+        <Link href={contactInfo.instagram || ""} target="_blank">
+          <Instagram color="white" size={32} />
+        </Link>
+        <Link href={contactInfo.linkedIn || ""} target="_blank">
+          <Linkedin color="white" size={32} />
+        </Link>
+      </div>
+    </motion.div>
+  );
 }
