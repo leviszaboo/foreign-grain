b7ea1d059a3372cda618fcc98a1b621e6c529276
Levi <leviszaboo@icloud.com>
2025-05-04 23:21:47 +0200

    fix: change maxDuration of Vercel functions

--- app/(routes)/digital/page.jsx
+++ app/(routes)/digital/page.jsx
@@ -6,6 +6,9 @@ import SmallHeader from "@/app/components/Header/SmallHeader";
 import ProjectGrid from "@/app/components/Work/Grid/ProjectGrid";
 
 export const revalidate = 0;
+export const runtime = "edge";
+export const preferredRegion = "home";
+export const maxDuration = 60;
 
 export default async function Digital() {
   const ref = `${process.env.NEXT_PUBLIC_USER_UID}/gallery/digital`;
